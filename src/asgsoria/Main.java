ce1223f69af357e2560cd172c5b83fea63ee7957
RSginer <=r.solerginer@gmail.com>
2017-01-20 21:22:56 +0100

    partially implemented game

--- src/asgsoria/Main.java
+++ src/asgsoria/Main.java
@@ -7,6 +7,9 @@ package asgsoria;
 
 import asgsoria.modelo.Barco;
 import asgsoria.modelo.Jugador;
+import java.util.Scanner;
+import java.util.Vector;
+
 /**
  *
  * @author Rubén
@@ -17,11 +20,40 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Barco barco = new Barco(1,"barco de alvaro",3,2);
-        barco.zarparDelPuerto();
-        barco.zarparDelPuerto(20);
-        Jugador alvaro = new Jugador(1, "Alvaro");
-        System.out.println(alvaro.getNombre());
+        Jugador jugadorRojo = new Jugador(1, "Jugador Rojo");
+        Jugador jugadorAzul = new Jugador(2, "Jugador azul");
+        Scanner scanner = new Scanner(System.in);
+        String respuestaUsuario = null;
+        do {
+            System.out.println("------------------------------------------");
+            System.out.println("|   MENU HUNDE LA FLOTA by AsgSoria =) !  |");
+            System.out.println("------------------------------------------");
+            System.out.println("1. Jugar");
+            System.out.println("2. Salir");
+            System.out.print("Introduce tu seleccion: ");
+            respuestaUsuario = scanner.nextLine();
+            switch (respuestaUsuario) {
+                case "1": {
+                    jugar(jugadorRojo, jugadorAzul);
+                    break;
+                }
+            }
+        } while (!respuestaUsuario.equals("2"));
+
     }
-    
+
+    private static void jugar(Jugador jugadorRojo, Jugador jugadorAzul) {
+        Barco mapaJugadorRojo[] = {null, null, null, null, null};
+        Barco mapaJugadorAzul[] = {null, null, null, null, null};
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("HOLA JUGADOR ROJO");
+        System.out.println("Introduce la posicion donde quieres poner tu barco: ");
+        String respuestaUsuario = scanner.nextLine();
+        mapaJugadorRojo[Integer.parseInt(respuestaUsuario)] = new Barco();
+        System.out.println("HOLA JUGADOR AZUL");
+        System.out.println("Introduce la posicion donde quieres poner tu barco: ");
+        respuestaUsuario = scanner.nextLine();
+        mapaJugadorAzul[Integer.parseInt(respuestaUsuario)] = new Barco();
+    }
+
 }
